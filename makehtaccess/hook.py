42171eff4b279e7297d821b63651284bd4da55c0
Lucas Rolff <Lucas.Rolff@spilgames.com>
2012-10-15 08:39:32 +0200

    Cleaned up code

--- makehtaccess/hook.py
+++ makehtaccess/hook.py
@@ -3,14 +3,10 @@ import sys
 
 rawData = sys.stdin.readlines()
 
-#print rawData[0]
 hookdata = eval(rawData[0].replace(':null', ':None'))
-context = hookdata['context']
 data = hookdata['data']
-
 username = data['user']
 domain = data['domain']
-
 
 f = open('/home/%s/.htaccess' % username, 'w')
 f.write('<IfModule mod_php5.c>\n')
